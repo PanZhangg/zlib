c138efdf3b592f7e8a8fc5259166b2989ff3d268
PanZhangg <dazhangpan@gmail.com>
2018-05-19 23:47:20 +0800

    simple test

--- tests/z_cirbuf_test.c
+++ tests/z_cirbuf_test.c
@@ -4,10 +4,16 @@
 
 int main() {
     struct z_cirbuf *cb = z_cirbuf_create(4096);
-    printf("size of int: %d\n", sizeof(int));
-    printf("size of z_cirbuf: %d\n", sizeof(struct z_cirbuf));
-    printf("size of void *: %d\n", sizeof(void *));
     if (cb == NULL) {
+        fprintf(stderr, "circular buffer create failed\n");
+        exit(0);
     }
+    char *produce_data = "Produce data";
+    char consume_buffer[20];
+    int ret = z_cirbuf_produce(cb,(void *)produce_data, sizeof(*produce_data));
+    ret = z_cirbuf_consume(cb, (void*)consume_buffer, sizeof(*produce_data));
+
+    printf("Text: %s\n", consume_buffer);
+    z_cirbuf_destroy(cb);
     return 0;
 }
